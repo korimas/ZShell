b260592f54c5f450f579170aca2d95313da6ee68
zpdev <himoker@163.com>
2019-12-19 15:35:10 +0800

    add pyinstaller spec file

--- core/app.py
+++ core/app.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QApplication
 from zshell.core.window.main import ZShellWindow
 import sys
+from PyQt5.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
 main_win = ZShellWindow()
